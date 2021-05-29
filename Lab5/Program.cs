b4a89086b34ab4b904b9bfbf5cbc1a6d1fbc4b6b
Григорьев Александр <foxamurai@gmail.com>
2021-05-29 12:47:20 +0400

    Final

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -8,14 +8,26 @@ namespace Lab5
         {
             Queries queries = new Queries();
 
-            //queries.Q1(1000000, 5000000, "Советский");
-            //queries.Q2(4);
-            //queries.Q3(4);
-            //queries.Q4(2,"Советский");
-            //queries.Q5("Иванов");
-            //queries.Q6("Кировский");
-            //queries.Q7(2);
+            queries.Q1(1000000, 5000000, "Советский");
+            queries.Q2(4);
+            queries.Q3(4);
+            queries.Q4(2,"Советский");
+            queries.Q5("Иванов");
+            queries.Q6("Кировский");
+            queries.Q7(2);
             queries.Q8("Первичное", "Иванов", "Безопасность");
+            queries.Q9("Первичное", "20.04.2021", "30.04.2021");
+            queries.Q10();
+            queries.Q11("Первичное");
+            queries.Q12(2);
+            queries.Q13();
+            queries.Q14("Кировский");
+            queries.Q15("Кировский");
+            queries.Q16("Иванов");
+            queries.Q17("Иванов",2021);
+            queries.Q18();
+            queries.Q19(2021);
+            queries.Q20();
         }
     }
 }
--- Lab5/Queries.cs
+++ Lab5/Queries.cs
@@ -21,6 +21,7 @@ namespace Lab5
             var result = context.RealEstateObjects.Include(reo => reo.District)
                 .Where(reo => reo.District.Name == districtName & reo.Price >= startPrice & reo.Price <= endPrice);
 
+            Console.WriteLine("Q1");
             foreach (var item in result)
             {
                 Console.WriteLine($"Адрес: {item.Address} \n" +
@@ -36,6 +37,7 @@ namespace Lab5
                 .Where(x => x.RealEstateObject.RoomsNumber == roomsNumber)
                 .Select(x => x.Realtor.Surname + " " + x.Realtor.Name + " " + x.Realtor.Patronymic);
 
+            Console.WriteLine("Q2");
             foreach (var item in result)
             {
                 Console.WriteLine(item + "\n");
@@ -55,6 +57,7 @@ namespace Lab5
                     Rieltor = x.Realtor.getFullName()
                 });
 
+            Console.WriteLine("Q3");
             foreach (var item in result)
             {
                 Console.WriteLine($"Адрес: {item.Address} \n" +
@@ -70,7 +73,8 @@ namespace Lab5
                 .Where(reo => reo.District.Name == districtName & reo.RoomsNumber == roomsNumber)
                 .Sum(x => x.Price);
 
-            Console.WriteLine(result);
+            Console.WriteLine("Q4");
+            Console.WriteLine(result + "\n\n");
         }
 
         public void Q5(string rieltorSurname)
@@ -79,8 +83,9 @@ namespace Lab5
                 .Include(s => s.Realtor)
                 .Where(s => s.Realtor.Surname == rieltorSurname);
 
+            Console.WriteLine("Q5");
             Console.WriteLine($"Минимум: {sales.Min(s => s.Price)} \n" +
-                $"Максимум: {sales.Max(s => s.Price)}");
+                $"Максимум: {sales.Max(s => s.Price)} \n\n");
         }
 
         public void Q6(string districtName)
@@ -89,7 +94,8 @@ namespace Lab5
                 .Where(e => e.RealEstateObject.District.Name == districtName)
                 .Average(x => x.EvaluationResult);
 
-            Console.WriteLine(result);
+            Console.WriteLine("Q6");
+            Console.WriteLine(result + "\n\n");
         }
 
         public void Q7(int floor)
@@ -103,29 +109,270 @@ namespace Lab5
                     count = x.Count()
                 });
 
+            Console.WriteLine("Q7");
             foreach (var item in result)
             {
                 Console.WriteLine($"{item.district}: {item.count}");
             }
+            Console.WriteLine("\n\n");
         }
 
+        //TO-DO
         public void Q8(string reoType, string rieltorSurname, string evalCriteria)
         {
             var sales = context.Sales
                 .Where(s => s.Realtor.Surname == rieltorSurname)
-                .Where(s => s.RealEstateObject.RealEstateType.Name == reoType)
-                .Include(x => x.RealEstateObject);
-             //   .Select(s => s.RealEstateObject.Evaluations
-             //       .Where(e => e.EvaluationCriteria.Name == evalCriteria));
-               
+                .Where(s => s.RealEstateObject.RealEstateType.Name == reoType);
 
-
-
+            Console.WriteLine("Q8");
             foreach (var item in sales)
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("\n\n");
+        }
+
+        public void Q9(string reoType, String starDate, String endDate)
+        {
+            DateTime sd = DateTime.Parse(starDate);
+            DateTime ed = DateTime.Parse(endDate);
+            var result = context.Sales
+                .Where(x => x.RealEstateObject.RealEstateType.Name == reoType)
+                .Where(x => x.SaleDate >= sd & x.SaleDate <= ed)
+                .Select(x => new
+                {
+                    priceForSqrMeter = x.Price/x.RealEstateObject.Area
+                }).Average(x => x.priceForSqrMeter);
+
+            Console.WriteLine("Q9");
+            Console.WriteLine(Math.Round(result,2) + "\n\n");
+
+        }
+
+        public void Q10()
+        {
+            var result = context.Realtors
+                .Select(x => new
+                {
+                    realtor = x.getFullName(),
+                    salesCounter = x.Sales.Count(),
+                    salesSum = x.Sales.Sum(x => x. Price)
+                });
+
+
+            Console.WriteLine("Q10");
+            foreach (var item in result)
+            {
+                string line = item.realtor + " - ";
+                if (item.salesCounter > 0)
+                {
+                    line += (item.salesCounter * item.salesSum * 0.05) / 100 * 87;
+                }
+                else
+                {
+                    line += "остался без премии";
+                }
+                line += "\n";
+                Console.WriteLine(line);                
+            }
+
+            Console.WriteLine("\n\n");
+
+        }
+
+        public void Q11(string reoType)
+        {
+            var result = context.Realtors
+                .Select(x => new 
+                {
+                    realtor = x.getFullName(),
+                    salesCounter = x.Sales.Count(x => x.RealEstateObject.RealEstateType.Name == reoType)
+                });
+
+            Console.WriteLine("Q11");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.realtor + " - " + item.salesCounter + "\n");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void Q12(int floor)
+        {
+            var result = context.RealEstateObjects
+                .Where(x => x.Floor == floor)
+                .GroupBy(x => x.BuildingMaterial.Name)
+                .Select(x => new
+                {
+                    material = x.Key,
+                    avg = x.Select(x => x).Average(x => x.Price)
+                });
+
+            Console.WriteLine("Q12");
             
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.material + " - " + item.avg + "\n");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void Q13()
+        {
+            var result = context.RealEstateObjects
+                .Select(x => x)
+                .AsEnumerable()
+                .GroupBy(x => x.DistrictId)
+                .Select(x => new 
+                { 
+                    district = context.Districts.First(y => y.Id == x.Key).Name,
+                    reos = x.Select(x => x).OrderByDescending(x => x.Price).ThenBy(x => x.Floor).Take(3)
+                });
+
+            Console.WriteLine("Q13");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.district);
+                foreach (var reo in item.reos)
+                {
+                    Console.WriteLine($"\t{reo.Address}, {reo.Price}, {reo.Floor}\n");
+                }
+                
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Q14(string districtName) 
+        {
+            var result = context.RealEstateObjects
+                .Where(x => x.Status == true & x.District.Name == districtName)
+                .Select(x => x.Address);
+
+            Console.WriteLine("Q14");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Q15(string districtName) 
+        {
+            var result = context.Sales
+                .Where(x => x.RealEstateObject.District.Name == districtName)
+                .Where(x => Math.Abs(x.Price - x.RealEstateObject.Price) <= x.RealEstateObject.Price / 100 * 80)
+                .Select(x => new 
+                {
+                    address = x.RealEstateObject.Address,
+                    district = x.RealEstateObject.District.Name
+                });
+
+            Console.WriteLine("Q15");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.address + " - " + item.district);
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Q16(string rielstorSurname)
+        {
+            var result = context.Sales
+                .Where(x => x.Realtor.Surname == rielstorSurname)
+                .Where(x => (x.RealEstateObject.Price - x.Price) > 100000)
+                .Select(x => new
+                {
+                    address = x.RealEstateObject.Address,
+                    district = x.RealEstateObject.District.Name
+                });
+
+            Console.WriteLine("Q16");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.address + " - " + item.district);
+            }
+            Console.WriteLine("\n");
+
+        }
+
+        public void Q17(string rielstorSurname, int year)
+        {
+            var result = context.Sales
+                .Where(x => x.Realtor.Surname == rielstorSurname)
+                .Where(x => x.SaleDate.Year == year)
+                .Select(x => new 
+                {
+                    address = x.RealEstateObject.Address,
+                    diff = (x.Price / x.RealEstateObject.Price - 1) * 100
+                });
+            Console.WriteLine("Q17");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.address + " : " + Math.Round(item.diff,2) + "%\n");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Q18()
+        {
+            var distrGrouping = context.RealEstateObjects
+                .AsEnumerable()
+                .GroupBy(x => x.District.Name);
+
+
+            Console.WriteLine("Q18");
+            foreach (var item in distrGrouping)
+            {
+                Console.WriteLine(item.Key);
+
+                var avgPrice = item.Select(x => x.Price / x.Area).Average();
+
+                var result = item.Where(x => (x.Price / x.Area) < avgPrice);
+
+                foreach (var reo in result)
+                {
+                    Console.WriteLine("\t" + reo.Address);
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void Q19(int year)
+        {
+            var result = context.Realtors
+                .Where(x => x.Sales.Where(y => y.SaleDate.Year == year).Count() == 0);
+
+            Console.WriteLine("Q19");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.getFullName());
+            }
+            Console.WriteLine();
+        }
+
+        public void Q20() 
+        {
+            var distrGrouping = context.RealEstateObjects
+                .Where(x => (x.AdDate.Month - DateTime.Today.Month) <= 4)
+                .AsEnumerable()
+                .GroupBy(x => x.District.Name);
+
+
+            Console.WriteLine("Q20");
+            foreach (var item in distrGrouping)
+            {
+                Console.WriteLine(item.Key);
+
+                var avgPrice = item.Select(x => x.Price / x.Area).Average();
+
+                var result = item.Where(x => (x.Price / x.Area) < avgPrice);
+
+                foreach (var reo in result)
+                {
+                    Console.WriteLine("\t" + reo.Address + " : " + reo.Status);
+                }
+            }
+            Console.WriteLine("\n");
         }
     }
 }
